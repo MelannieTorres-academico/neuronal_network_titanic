39622773e46b59f45fa05fdff4e224bb41cf9193
Melannie Torres <melannie@mienvio.mx>
2019-03-24 17:09:24 -0600

    dimensionality, k-folds and refactor

--- ann.py
+++ ann.py
@@ -1,5 +1,3 @@
-#https://towardsdatascience.com/how-to-build-your-own-neural-network-from-scratch-in-python-68998a08e4f6
-#https://blog.zhaytam.com/2018/08/15/implement-neural-network-backpropagation/
 import numpy as np
 import pandas as pd
 import math
@@ -22,19 +20,18 @@ def sigmoid_derivative(x):
 # Scales the given data using regularization
 # params:
 #   column: a set of values
-# The set of values scaled
+# returns: The set of values scaled
 def regularization(column):
     aux_column = []
     for value in column:
-        aux_column.append((value-mean(column))/stdev(column)) #https://maristie.com/blog/differences-between-normalization-standardization-and-regularization/
-        #https://medium.com/greyatom/why-how-and-when-to-scale-your-features-4b30ab09db5e
+        aux_column.append((value-mean(column))/stdev(column))
     return aux_column
 
 # Applies one_hot_encoding to the column_name from X
 # params:
 #   X: the DataFrame with the input data
 #   column_name: the name of the column that wants to be 'one-hot-encoded'
-# The dataframe with the new columns of the one_hot_encoding
+# returns: The dataframe with the new columns of the one_hot_encoding
 def one_hot_encoding(X, column_name):
     possible_values = X[column_name].unique()
     aux_matrix = [[0]*len(possible_values)]*len(X[column_name]) #matrix of zeros
@@ -46,6 +43,69 @@ def one_hot_encoding(X, column_name):
     X = X.drop(column_name, axis=1)
     X = pd.concat([X, df], axis=1, join_axes=[X.index])
     return X
+
+# Applies k-folds cross-validation
+# params:
+#   X: the full input DataFrame
+#   y: the full input DataFrame
+#   k: number of partitions
+# returns: no return
+def k_folds(X, y, k):
+    accuracies = []
+    chunks_x = split(X, int(len(X)/k))
+    chunks_y = split(y, int(len(y)/k))
+    for i in range(k):
+        X = []
+        y = []
+        first_time = True
+        for j in range(k):
+            if(j!=i):
+                if (first_time):
+                    X = chunks_x[j]
+                    y = chunks_y[j]
+                    first_time = False
+                else:
+                    X=X.append(chunks_x[j], ignore_index=True)
+                    y=y.append(chunks_y[j], ignore_index=True)
+            else:
+                cross_validation_x = chunks_x[j]
+                cross_validation_y = chunks_y[j]
+        f = open("k-folds/k-folds.txt","a")
+        f.write('\nk = %d\n' %i)
+        nn = NeuralNetwork(X, y, 3)
+        epochs = 2
+        nn.run_nn_simulation(X, y, epochs, f, True)
+        accuracy = nn.check_accuracy(cross_validation_x, cross_validation_y)
+        accuracies.append(accuracy)
+        f.write("accuracy %f\n" %(accuracy))
+    f.write('AVG accuracies %f' %mean(accuracies))
+
+# Splits a dataframe creating new dataframes of size chunk_size
+# params:
+#   df: the dataframe
+#   chunk_size: the size of the new dataframes
+# return:
+#   the new dataframes
+# function retrieved from: http://yaoyao.codes/pandas/2018/01/23/pandas-split-a-dataframe-into-chunks
+def split(df, chunk_size):
+    indices = range(1 * chunk_size, (df.shape[0] // chunk_size + 1) * chunk_size, chunk_size)
+    return np.split(df, indices)
+
+# Cleans the dataframe given according to the results from the experimentation
+# params:
+#   df: the dataframe to be cleaned
+# return:
+#   df: the dataframe cleaned
+def clean_dataset(df):
+    #one hot encoding
+    df = one_hot_encoding(df, "Pclass")
+    #dimensionality reduction - consult atached files to see why I deleted this columns
+    df = df.drop('Parch', axis=1)
+    df = df.drop(3, axis=1)
+    #regularization
+    for column in df:
+        df[column] = regularization(df[column])
+    return df
 
 class NeuralNetwork:
     def __init__(self, x, y, n_layers):
@@ -73,14 +133,21 @@ class NeuralNetwork:
     #   id_row: the id corresponding to the current row from the input
     # return: no return
     def feedforward(self, id_row):
-        current_y = self.y.iloc[id_row,:]['Survived']
         current_x = self.input.iloc[id_row,:]
-        last_layer_index = len(self._layers)-1
         for i in range(len(self._layers)):
             if (i == 0 ): #if first layer
                 self._layers[i]= sigmoid(np.dot(current_x, self.weights[i]))
             else:
                 self._layers[i] = sigmoid(np.dot(self._layers[i-1], self.weights[i]))
+    # Prepares the data to feedforward the nn during the training
+    # and calls the feedforward function
+    # params:
+    #   id_row: the id corresponding to the current row from the input
+    # return: no return
+    def _feedforward(self, id_row):
+        current_y = self.y.iloc[id_row,:]['Survived']
+        last_layer_index = len(self._layers)-1
+        self.feedforward(id_row)
         self.sq_mean_error = 0.5 *((current_y - self._layers[last_layer_index]) ** 2)
         self.cross_entropy  = -current_y*math.log(self._layers[last_layer_index])-(1-current_y)*math.log(1-self._layers[last_layer_index])
 
@@ -108,36 +175,99 @@ class NeuralNetwork:
             input_to_use = np.atleast_2d(current_x if i == 0 else self._layers[i - 1]) #https://docs.scipy.org/doc/numpy/reference/generated/numpy.atleast_2d.html
             self.weights[i] += self.deltas[i] * input_to_use.T * self.learning_rate
 
+    # Evaluates the accuracy of the model
+    # params:
+    #   X: unseen input data to do cross-validation
+    #   y: unseen results of the X
+    # return:
+    #   accuracy rate
+    def check_accuracy(self, X, y):
+        m = X.shape[0] #number of samples
+        n_right = 0
+        n_wrong = 0
+        for j in range(m):
+            result = self.test_value(j, X)
+            if(y.iloc[j,:]['Survived'] == result):
+                n_right += 1
+            else:
+                n_wrong += 1
+        return n_right/(n_right+n_wrong)
+
+    # Runs all the tests
+    # params:
+    #   test: the tests dataframe
+    # return:
+    #   no return
+    def run_tests(self, test):
+        m = test.shape[0] #number of samples
+        for j in range(m):
+            result = self.test_value(j, test)
+            print(result)
+
+    # Evaluates the given value
+    # params:
+    #   id_row: the id corresponding to the current row from the testing dataset
+    #   x_test: the testing dataset
+    # return: the result of the evaluation
+    def test_value(self, id_row, x_test):
+        # current_x = x_test.iloc[id_row,:]
+        last_layer_index = len(self._layers)-1
+        self.feedforward(id_row)
+        estimation = self._layers[last_layer_index]
+        result = 1 if estimation > 0.5 else 0
+        return result #last layer
+
+
+    # runs a simulation of the nn with the given input data X
+    # params:
+    #   X: the DataFrame with the input data
+    #   y: the expected results
+    #   n_epochs: number of epochs to be run
+    #   f: file where all the info of the simulations will be saved
+    def run_nn_simulation(self, X, y, n_epochs, f, print_weights = False):
+        self.add_layer(X.shape[1],3) #X.shape[1] is num of input cols
+        self.add_layer(3,2)
+        self.add_layer(2,1)
+        m = X.shape[0] #number of samples
+        for i in range(n_epochs):
+            sq_mean_error = 0
+            cross_entropy = 0
+            for j in range(m):
+                self._feedforward(j)
+                sq_mean_error += self.sq_mean_error
+                cross_entropy += self.cross_entropy
+                self.backprop(j)
+        f.write("cost: square mean error: %f\n" %(sq_mean_error[0]))
+        f.write("cost: cross-entropy: %f\n" %(cross_entropy/m))
+
+        if (print_weights):
+            f.write("weights: "+str(self.weights)+"\n")
+
 
 def main():
     X = pd.read_csv('../titanic/train_x.csv', sep=',', skiprows=1, names=['Pclass', 'Sex', 'Age', 'Parch', 'Fare'])
     y = pd.read_csv('../titanic/train_y.csv', sep=',', skiprows=1, names=['Survived'])
+    test = pd.read_csv('../titanic/test.csv', sep=',', skiprows=1, names=['Pclass', 'Sex', 'Age', 'Parch', 'Fare'])
 
-    #one hot encoding
-    X = one_hot_encoding(X, "Pclass")
+    # clean_datasets
+    X = clean_dataset(X)
+    test = clean_dataset(test)
 
-    #regularization
-    for column in X:
-        X[column] = regularization(X[column])
-
-    nn = NeuralNetwork(X, y, 3)
-    nn.add_layer(X.shape[1],4) #X.shape[1] is num of input cols
-    nn.add_layer(4,3)
-    nn.add_layer(3,1)
-
+    #k-folds
+    # k_folds(X, y, 10)
     m = X.shape[0] #number of samples
-    for i in range(1):#0000):
-        sq_mean_error = 0
-        cross_entropy = 0
-        for j in range(m):
-            nn.feedforward(j)
-            sq_mean_error += nn.sq_mean_error
-            cross_entropy += nn.cross_entropy
-            nn.backprop(j)
 
-    print("cost: square mean error: ",sq_mean_error[0])
-    print("cost: cross-entropy: ",(cross_entropy/m))
+    f = open("training/training.txt","a")
 
-    print("Weights: ",nn.weights)
+    X_80_20 = split(X, int(0.8*m))
+    y_80_20 = split(y, int(0.8*m))
+    nn = NeuralNetwork(X, y, 3)
+    epochs = 50
+    nn.run_nn_simulation(X_80_20[0], y_80_20[0], epochs, f, True)
+    nn.check_accuracy(X_80_20[1], y_80_20[1])
+
+    nn.run_tests(test)
+
+    print('\a')
 if __name__ == "__main__":
     main()
--- dimentionality_functions.py
+++ dimentionality_functions.py
@@ -6,7 +6,7 @@ import pandas as pd
 #   y: the expected results
 def dimensionality_reduction(X, y):
     column_names = list(X.columns.values)
-    _all_combinations = all_combinations(X, column_names)
+    _all_combinations = all_combinations(X)
     n_rows = len(_all_combinations)
     n_cols = len(_all_combinations.iloc[0])
     for i in range(n_rows):
@@ -15,11 +15,11 @@ def dimensionality_reduction(X, y):
             if(_all_combinations.iloc[i][column_names[j]] == 0):
                 #drop colum
                 aux_X = aux_X.drop(column_names[j], axis=1)
-        f= open("dimensionality_results.txt","a")
-        f2= open("only_squared_mean_error.txt","a")
-        f3= open("only_cross_error.txt","a")
+        f= open("dimensionality/dimensionality_results.txt","a")
         f.write(str(len(list(aux_X.columns.values)))+' :'+str(list(aux_X.columns.values))+' \n')
-        run_nn(aux_X, y, f, f2, f3, epochs)
+        epochs = 2
+        nn = ann.NeuralNetwork(aux_X, y, 3)
+        nn.run_nn_simulation(aux_X, y, epochs, f)
 
 # calculates all the possible combinations of which columns to use for a model
 # params:
@@ -51,33 +51,6 @@ def half_1_half_0(start, end, combinations, column_names, value, current_col):
         middle=start+int((end-start)/2)
         half_1_half_0(start, middle, combinations, column_names, 1, current_col+1)
         half_1_half_0(middle, end, combinations, column_names, 0, current_col+1)
-
-# runs a simulation of the nn with the given input data X
-# params:
-#   X: the DataFrame with the input data
-#   y: the expected results
-#   n_epochs: number of epochs to be run
-#   f: file where all the info of the simulations will be saved
-#   f2: file where the square mean errors will be saved
-#   f3: file where the cross-entropy errors will be saved
-def run_nn(X, y, n_epochs, f, f2, f3):
-    nn = ann.NeuralNetwork(X, y, 3)
-    nn.add_layer(X.shape[1],4) #X.shape[1] is num of input cols
-    nn.add_layer(4,3)
-    nn.add_layer(3,1)
-    m = X.shape[0] #number of samples
-    for i in range(n_epochs):
-        sq_mean_error = 0
-        cross_entropy = 0
-        for j in range(m):
-            nn.feedforward(j)
-            sq_mean_error += nn.sq_mean_error
-            cross_entropy += nn.cross_entropy
-            nn.backprop(j)
-    f.write("cost: square mean error: %d\n" %(sq_mean_error[0]))
-    f.write("cost: cross-entropy: %d\n" %(cross_entropy/m))
-    f2.write(", %d" %(sq_mean_error[0]))
-    f3.write(", %d" %((cross_entropy/m)))
 
 
 def main():
